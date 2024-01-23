3ae9163dbad2a36b7b7617316f0bd768ebdff526
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-01-23 19:55:57 +0800

    Update generate_sgmodule.py

--- .github/scripts/generate_sgmodule.py
+++ .github/scripts/generate_sgmodule.py
@@ -12,12 +12,21 @@ def js_to_sgmodule(js_content):
     rewrite_match = re.search(r'\[rewrite_local\]\s*(.*?)\s*\[mitm\]\s*hostname\s*=\s*(.*?)\s*', js_content, re.DOTALL | re.MULTILINE)
     mitm_match = re.search(r'\[mitm\]\s*([^=\n]+=[^\n]+)\s*', js_content, re.DOTALL | re.MULTILINE)
     hostname_match = re.search(r'hostname\s*=\s*([^=\n]+=[^\n]+)\s*', js_content, re.DOTALL | re.MULTILINE)
-    
-    if not (name_match and desc_match):
-        raise ValueError("Invalid JS file format")
 
-    project_name = name_match.group(1).strip()
-    project_desc = desc_match.group(1).strip()
+    # If there is no project name and description, use the last part of the matched URL as the project name
+    if not (name_match and desc_match):
+        url_pattern = r'(url\s+script-(?:response|request|echo-response|request-header|response-header|analyze-echo-response)\s+\S+\s+(.*))'
+        last_part_match = re.search(url_pattern, js_content, re.DOTALL | re.MULTILINE)
+        if last_part_match:
+            project_name = os.path.splitext(os.path.basename(last_part_match.group(2).strip()))[0]
+        else:
+            raise ValueError("Invalid JS file format")
+        
+        project_desc = f"Generated from {project_name}"
+
+    else:
+        project_name = name_match.group(1).strip()
+        project_desc = desc_match.group(1).strip()
 
     rewrite_local_content = rewrite_match.group(1).strip()
     mitm_content = mitm_match.group(1).strip() if mitm_match else ''
@@ -27,7 +36,7 @@ def js_to_sgmodule(js_content):
     mitm_content_with_append = insert_append(mitm_content)
 
     # Extract pattern and script type from rewrite_local_content
-    pattern_script_match = re.search(r'^(.*?)\s*url\s+script-(response-body|request-body|echo-response|request-header|response-header|analyze-echo-response)\s+(.*)$', rewrite_local_content, re.MULTILINE)
+    pattern_script_match = re.search(r'^(.*?)\s*url\s+script-(response|request|echo-response|request-header|response-header|analyze-echo-response)\s+(.*)$', rewrite_local_content, re.MULTILINE)
     if not pattern_script_match:
         raise ValueError("Invalid rewrite_local format")
 
@@ -65,7 +74,7 @@ def main():
                 # Write sgmodule content to surge folder
                 surge_folder_path = 'surge'
                 os.makedirs(surge_folder_path, exist_ok=True)
-                sgmodule_file_path = os.path.join(surge_folder_path, f"{file_name.split('.')[0]}.sgmodule")
+                sgmodule_file_path = os.path.join(surge_folder_path, f"{os.path.splitext(file_name)[0]}.sgmodule")
                 with open(sgmodule_file_path, "w", encoding="utf-8") as sgmodule_file:
                     sgmodule_file.write(sgmodule_content)
 
