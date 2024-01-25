c3fc7b8f36555cff9c5dc62055e650d8ddccb1f2
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-01-25 20:12:21 +0800

    Update generate_sgmodule.py

--- .github/scripts/generate_sgmodule.py
+++ .github/scripts/generate_sgmodule.py
@@ -78,7 +78,7 @@ def js_to_sgmodule(js_content):
     rewrite_local_content = rewrite_local_match.group(1).strip()
 
     # Extract pattern and script type from rewrite_local_content
-    pattern_script_matches = re.finditer(r'^(.*?)\s*url\s+script-(response-body|request-body|echo-response|request-header|response-header|analyze-echo-response)\s+(\S+.*?)$', rewrite_local_content, re.MULTILINE)
+    pattern_script_matches = re.finditer(r'^(.*?)\s*(?:url\s+script-(response-body|request-body|echo-response|request-header|response-header|analyze-echo-response)\s+(\S+.*?)$)', rewrite_local_content, re.MULTILINE)
 
     if not pattern_script_matches:
         raise ValueError("Invalid rewrite_local format")
