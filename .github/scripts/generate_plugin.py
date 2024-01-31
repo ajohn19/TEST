b541b955e017095faada6169ad2dada6e27d87d5
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-01-31 16:27:15 +0800

    Update generate_plugin.py

--- .github/scripts/generate_plugin.py
+++ .github/scripts/generate_plugin.py
@@ -63,33 +63,38 @@ def extract_rules(js_content):
 
     rewrite_local_content = rewrite_local_match.group(1).strip()
     url_script_matches = re.finditer(
-        r'^#.*?(https?://[^\s]+)\s+url\s+script-(response|request)-(?:body|header)\s+(https?://[^\s]+)\s*,\s*tag\s*=\s*(\S+)',
+        r'^([^#\n].*?)\s+url\s+script-(response|request)-(?:body|header)\s+(https?://[^\s]+)\s*,\s*tag\s*=\s*(\S+)',
         rewrite_local_content, re.MULTILINE
     )
 
     scripts = "[Script]\n"
     for match in url_script_matches:
-        section, type, script_path, tag = match.groups()
+        pattern, type, script_path, tag = match.groups()
         last_url_segment = os.path.splitext(os.path.basename(script_path))[0]
-        scripts += f"http-{type} {section} {script_path} tag={tag}\n"
+        scripts += f"http-{type} {last_url_segment} {pattern} script-path={script_path} tag={tag}\n"
 
     return scripts
 
 # Repository structure and file conversion
 base_path = os.getcwd()
 qx_path = os.path.join(base_path, 'qx')
+loon_path = os.path.join(base_path, 'loon')
+
+# Create 'loon' directory if it doesn't exist
+if not os.path.exists(loon_path):
+    os.makedirs(loon_path)
 
 # Loop through all files in 'qx' directory
 for root, dirs, files in os.walk(qx_path):
     for file_name in files:
-        if file_name.lower().endswith('.js'):
+        if file_name.lower().endswith(('.js', '.conf', '.snippet')):
             file_path = os.path.join(root, file_name)
             with open(file_path, 'r') as file:
                 js_content = file.read()
 
             loon_plugin_content = convert_js_to_loon(js_content)
             output_file_name = f"{os.path.splitext(file_name)[0]}.plugin"
-            output_file_path = os.path.join(base_path, output_file_name)
+            output_file_path = os.path.join(loon_path, output_file_name)
             with open(output_file_path, 'w') as file:
                 file.write(loon_plugin_content)
             print(f"Converted: {file_name} -> {output_file_name}")
