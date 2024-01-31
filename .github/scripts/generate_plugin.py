a1fa7a8ba0ee6c3050f92cc334165c9f3ecbb831
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-01-31 19:00:16 +0800

    Update generate_plugin.py

--- .github/scripts/generate_plugin.py
+++ .github/scripts/generate_plugin.py
@@ -37,6 +37,9 @@ def js_to_plugin(js_content):
 
     img_url_match = re.search(r'img-url\s*=\s*(https?://[^\s]+)', js_content)
     img_url = img_url_match.group(1) if img_url_match else ""
+    # Add the icon url to the plugin content if it exists
+    if img_url:
+        plugin_content += f"#!icon={img_url}\n"
     
     # If there is no project name and description, use the last part of the matched URL as the project name
     if not (name_match and desc_match):
@@ -61,9 +64,6 @@ def js_to_plugin(js_content):
     # Generate plugin content
     plugin_content = f"""#!name={project_name}
 #!desc={project_desc}
-    # Add the icon url to the plugin content if it exists
-    if img_url:
-        plugin_content += f"#!icon={img_url}\n"
 
 [MITM]
 {mitm_content_with_append}
