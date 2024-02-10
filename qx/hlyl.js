55306df05f3da14b1130468add05acafd2fa10fb
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-02-10 14:57:50 +0800

    Update hlyl.js

--- qx/hlyl.js
+++ qx/hlyl.js
@@ -23,7 +23,7 @@
 ------------------ Surge 配置 -----------------
 
 [Script]
-活力伊利获取token = type=http-request,pattern=https://zhls.qq.com/api/report?token=bifbdbfb7e1a6b4a93,requires-body=0,max-size=0,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js
+活力伊利获取token = type=http-request,pattern=https:\/\/msmarket\.msx\.digitalyili\.com\/gateway\/api\/member\/sign\/config,requires-body=0,max-size=0,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js
 
 活力伊利小程序签到 = type=cron,cronexp=1 9 * * *,timeout=60,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js,script-update-interval=0
 
