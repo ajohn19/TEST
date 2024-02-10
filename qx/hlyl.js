b76a7b9cbec005596d1406bcb8c7fc0049857fb2
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-02-10 15:39:17 +0800

    Update hlyl.js

--- qx/hlyl.js
+++ qx/hlyl.js
@@ -26,12 +26,12 @@
 ------------------ Surge 配置 -----------------
 
 [Script]
-活力伊利获取token = type=http-request,pattern=https:\/\/msmarket\.msx\.digitalyili\.com\/gateway\/api\/member\/sign\/config,requires-body=0,max-size=0,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js
+活力伊利获取token = type=http-request,pattern=https:\/\/digitaldmo\.yili\.com\/sa\?project=yilixcx,requires-body=0,max-size=0,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js
 
 活力伊利小程序签到 = type=cron,cronexp=1 9 * * *,timeout=60,script-path=https://raw.githubusercontent.com/ajohn19/TEST/main/qx/hlyl.js,script-update-interval=0
 
 [MITM]
-hostname = msmarket.msx.digitalyili.com
+hostname = digitaldmo.yili.com
 ====================================
 ⚠️【免责声明】
 ------------------------------------------
