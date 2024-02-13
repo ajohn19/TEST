dd790dd9d0760ff740cab28d21a97961b7831083
Ash John <154985771+ajohn19@users.noreply.github.com>
2024-02-13 23:41:40 +0800

    Update Anki.js

--- qx/Anki.js
+++ qx/Anki.js
@@ -11,10 +11,14 @@
 
  */
 var body = JSON.parse($response.body);
-
 body.data.is_vip = true;
 body.data.vip_expire_at = 4102403992;
 body.data.vip_end_at = 4102403992;
 body.data.vip_day = 999; 
-
 $done({ body: JSON.stringify(body) });
+
+
+// Quantumult X引用地址： https://raw.githubusercontent.com/ajohn19/TEST/main/qx/Aki.js
+// Surge/Shadowrocket 模块地址： https://raw.githubusercontent.com/ajohn19/TEST/main/surge/Aki.sgmodule
+// Loon 插件地址： https://raw.githubusercontent.com/ajohn19/TEST/main/loon/Aki.plugin
+// Stash 覆写地址： https://raw.githubusercontent.com/ajohn19/TEST/main/stash/Aki.stoverride
